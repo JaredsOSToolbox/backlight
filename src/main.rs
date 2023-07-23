41af66c59f97669ff7bdc345fefe74c2f336397e
JaredDyreson <jared.dyreson@gmail.com>
2023-07-22 19:32:19 -0700

    Added better control flow logic for each of the options

--- src/main.rs
+++ src/main.rs
@@ -10,17 +10,38 @@ struct Args {
     #[arg(long)]
     decrease: Option<u32>,
     #[arg(long)]
-    path: String,
+    brightness_path: String,
+    #[arg(long)]
+    max_brightness: Option<bool>,
+    #[arg(long)]
+    brightness: Option<u32>,
 }
 
 fn main() {
     let args = Args::parse();
-    let current_brightness = filesystem::current_brightness_level(&args.path).unwrap();
+    let current_brightness =
+        filesystem::current_brightness_level(&format!("{}/brightness", &args.brightness_path))
+            .expect("Could not get the current brightness level of your device");
+
     if let Some(increase) = args.increase {
-        set_brightness(current_brightness + increase, &args.path).unwrap();
+        set_brightness(current_brightness + increase, &args.brightness_path)
+            .expect(&format!("Could not increase your brightness by {increase}"));
     }
 
     if let Some(decrease) = args.decrease {
-        set_brightness(current_brightness - decrease, &args.path).unwrap();
+        set_brightness(current_brightness - decrease, &args.brightness_path)
+            .expect(&format!("Could not increase your brightness by {decrease}"));
+    }
+
+    if let Some(_) = args.max_brightness {
+        println!(
+            "Max brightness: {}",
+            filesystem::max_brightness_level(&format!("{}/max_brightness", &args.brightness_path))
+                .expect("Could not get the maximum brightness")
+        );
+    }
+    if let Some(value) = args.brightness {
+        set_brightness(value, &args.brightness_path)
+            .expect(&format!("Could not set brightness level to {value}"));
     }
 }
