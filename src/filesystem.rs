12db3aef10dbc59d3ddb551e4ab18cea6d4d8af7
JaredDyreson <jared.dyreson@gmail.com>
2023-07-22 19:31:58 -0700

    Added `set_brightness`

--- src/filesystem.rs
+++ src/filesystem.rs
@@ -1,5 +1,6 @@
 use std::fs::OpenOptions;
 use std::io::Write;
+
 /// Read the contents of a file that pertains to brightness control
 fn _read_helper(file_path: &str) -> Result<u32, Box<dyn std::error::Error>> {
     Ok(std::fs::read_to_string(file_path)?.trim().parse::<u32>()?)
@@ -12,7 +13,7 @@ fn _write_helper(file_path: &str, contents: String) -> Result<(), std::io::Error
         .write(true)
         .truncate(true)
         .open(file_path)?;
-    Ok(fd.write_all(contents.as_bytes())?)
+    Ok(fd.write_all(contents.as_bytes()).unwrap())
 }
 
 pub fn current_brightness_level(file_path: &str) -> Result<u32, Box<dyn std::error::Error>> {
@@ -23,9 +24,14 @@ pub fn max_brightness_level(file_path: &str) -> Result<u32, Box<dyn std::error::
     _read_helper(file_path)
 }
 
-pub fn set_brightness(value: u32, file_path: &str) -> Result<(), Box<dyn std::error::Error>> {
-    //let max_brightness =
-        //max_brightness_level("/sys/class/backlight/intel_backlight/max_brightness")?;
-    //Ok(0_usize)
-    Ok(_write_helper(file_path, format!("{value}\n"))?)
+pub fn set_brightness(
+    value: u32,
+    brightness_path: &str,
+) -> Result<(), Box<dyn std::error::Error>> {
+    let max_brightness = max_brightness_level(&format!("{brightness_path}/max_brightness"))?;
+    if value > max_brightness {
+        return Ok(());
+    }
+
+    Ok(_write_helper(&format!("{brightness_path}/brightness"), format!("{value}"))?)
 }
